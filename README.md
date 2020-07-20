1a1080684355fc344ccaff0197fe8e7bd2eb9ce8
Levin Batallones <levinbatallones@Batallones.local>
2020-07-20 14:19:23 -0700

    [readme] Add content of read me

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # JavaScript Review
 This is my review of JavaScript.
+
+## Example of Data types
+``` JavaScript
+    let name = "Levin Batallones";
+    let age = 25;
+    let isCool = true;
+    const bike = ['Yamaha', 'Honda', 'Kawasaki', 'Harley', 'Triump'];
+
+    const nba = {
+        Warriors: 'Steph Curry',
+        blazers: 'Damian Lillard',
+        chicagoBulls: ['Jordan', 'Pippen', 'Rodman', 'Kerr'],
+        bullsChampionship: 6,
+        lakers: {
+            legendOne: 'Kobe',
+            legendTwo: 'Magic',
+            legendThree: "O'Neal",
+        },
+        print function () {
+            console.log(this.blazers + " sent PG back to OKC")
+        }
+
+    }
+```
---
+++ app.js
@@ -0,0 +1,31 @@
+let name = "Levin Batallones";
+let age = 25;
+let isCool = true;
+const bike = ['Yamaha', 'Honda', 'Kawasaki', 'Harley', 'Triump'];
+
+const tesla = {
+    industry: 'Electric Cars',
+    ceo: 'Elon Musk',
+    yearFounded: 2003,
+    foundingMembers: ['Elon Musk', 'JB Straubol', 'Martin Eberard'],
+    stockTicker: 'TSLA',
+    vehicles: {
+        vehicleOne: 'Model S',
+        vehicleTwo: 'Model X',
+        vehicleThree: 'Model 3'
+    },
+    print: function() {
+        console.log(this.ceo + " is the CEO of Tesla");
+    }
+}
+
+tesla.stockPrice = 1643.00;
+tesla.stockPrice
+console.log(name);
+console.log(age);
+console.log(isCool);
+console.log(bike);
+console.log(tesla)
+console.log(tesla.vehicles.vehicleThree)
+
+tesla.print();
